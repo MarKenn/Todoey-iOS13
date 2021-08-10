00c06428cc9b12edf971a3d486e8aed72a302334
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-08-10 16:58:13 +0800

    Replace default VC with UITableViewController Initial content for table

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Mark Kenneth Bayona on 8/10/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let TodoListCellID = "ToDoItemCell"
+}
