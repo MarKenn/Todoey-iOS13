2c0f4d42bb232e74fd3720e106ab98acf547225d
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-08-10 18:29:38 +0800

    Organize + Item model

---
+++ Todoey/Models/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Mark Kenneth Bayona on 8/10/21.
+//  Copyright © 2021 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
